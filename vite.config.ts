449347ca5c559063cafe55e2e7c90dd39264a7e3
pacemankun <liukun0227@163.com>
2026-08-14 01:22:11 +0800

    chore: remove frontend test suite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    css: false,
-    environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts"],
-  },
 });
